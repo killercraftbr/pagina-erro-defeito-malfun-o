e3e214328b393f22b15d7dde0b174f531820c2bd
jose_Elias <elliasilva17@gmail.com>
2022-07-24 16:50:28 -0300

    pagina finalizada

---
+++ defeito.php
@@ -0,0 +1,1 @@
+<?php echo $nome; ?>" name="nome">
---
+++ falha.php
@@ -0,0 +1,11 @@
+<?php echo $nome; ?> 
+<head>
+   <script type = "text/javascript">
+        function fun() {
+           alert("não foi possivel concluir o cadastro, erro no servidor, contate o programado, para avisar sobre o erro");
+        }
+   </script>
+</head>
+<body>
+<input type="submit" name="submit" onclick="fun();
+
